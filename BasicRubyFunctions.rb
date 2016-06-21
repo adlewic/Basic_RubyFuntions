8d53c05c6947e177fe237a16a1642633f4d954d5
Imelda Alonso <adlewic@gmail.com>
2016-06-21 15:55:18 -0400

    trying the basics

---
+++ BasicRubyFunctions.rb
@@ -0,0 +1,41 @@
+# Create a function that takes a string and adds the phrase 
+# "Only in America!" to the end of it
+
+# def frase (s)
+# 	s+"Only in america!"
+# end
+# puts=frase("Venti size coffe ")
+
+
+# Create a function to find the maximum value in an array of numbers. 
+# For instance: [100,10,-1000]
+# def maxim()
+
+# valores =[3,100,20,25,120] 
+# end
+# puts maxim
+
+def valor_maximo(array)
+	
+	
+
+
+	valor_maximo=[3,100,20,25,120] 
+
+
+# name = [":Imelda",":Gino",":James"]
+# 	lastname = ["Alonso","Cortes", "Hwang"]	
+
+
+
+
+
+
+# ----------this works but we can't use .zip ----------
+def person (name, lastname)
+	puts people =name.zip(lastname)
+end
+person(["Imelda","James"],["Alonso","Hwang"])
+ 
+
+
